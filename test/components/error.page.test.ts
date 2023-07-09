74b4a1334ffa51635d3d4aa8c0250c8092cab758
Mathias Bosman <436498+mathiasbosman@users.noreply.github.com>
2023-07-09 18:11:51 +0200

    feat: use vitest for simple unit testing

--- test/components/error.page.test.ts
+++ test/components/error.page.test.ts
@@ -1,50 +1,11 @@
 import '../../src/components/error.page.js'
-import { describe, expect, it, beforeEach } from 'vitest'
-import { type SandboxError } from '../../src/components/error.page'
+import {describe, expect, it} from 'vitest'
+import {SandboxError} from "../../src/components/error.page";
 
-describe('<sandbox-error> default', () => {
-  function getElement (): SandboxError | null | undefined {
-    return document.body.querySelector('sandbox-error')
-  }
-
-  beforeEach(async () => {
-    document.body.innerHTML = '<sandbox-error></sandbox-error>'
-    await new Promise<void>((resolve) => {
-      const interval = setInterval(() => {
-        if (getElement() != null) {
-          clearInterval(interval)
-          resolve()
-        }
-      })
-    })
+describe('<sandbox-error>', () => {
+  it('default properties', () => {
+      const sandboxError = new SandboxError();
+      expect(sandboxError.errorCode).equal(500);
+      expect(sandboxError.errorText).equal('An error occurred');
   })
-
-  it('default attributes are set', () => {
-    const error = getElement()
-    expect(error?.errorCode).equal(500)
-    expect(error?.errorText).equal('An error occurred')
-  })
-
-/*
-  it('default attributes', async () => {
-    const error: SandboxError = await fixture(html` <sandbox-error></sandbox-error> `);
-    assert.equal(error.errorCode, 500);
-    assert.equal(error.errorText, 'An error occurred');
-  });
-
-  it('all attributes', async () => {
-    const error: SandboxError = await fixture(html` <sandbox-error errorCode="404" errorText="foo"></sandbox-error> `);
-    assert.equal(error.errorCode, 404);
-    assert.equal(error.errorText, 'foo');
-    assert.equal(error.innerHTML, '');
-  });
-
-  it('all attributes - with content', async () => {
-    const error: SandboxError = await fixture(html` <sandbox-error errorCode="404" errorText="foo">bar</sandbox-error> `);
-    assert.equal(error.errorCode, 404);
-    assert.equal(error.errorText, 'foo');
-    assert.equal(error.innerHTML, 'bar');
-  });
-
- */
 })
